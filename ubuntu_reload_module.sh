4cf9c0fc1a2b1dfea65177008c622f735169b723
Georg Graßnick <georg.grassnick@tu-dresden.de>
2022-03-14 14:22:02 +0000

    Make script more readable

--- ubuntu_reload_module.sh
+++ ubuntu_reload_module.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "none" | sudo tee /sys/class/block/nvme0n1/queue/scheduler && sudo modprobe -r k2 && sudo cp k2.c Makefile dkms.conf /usr/src/k2-0.0.1/ && install -m 644 k2_trace.h /lib/modules/$(uname -r)/build/include/trace/events/k2.h && sudo dkms remove k2/0.0.1 && sudo dkms install k2/0.0.1 && sudo modprobe k2 && echo "k2" | sudo tee /sys/class/block/nvme0n1/queue/scheduler
+echo "none" | sudo tee /sys/class/block/nvme0n1/queue/scheduler ;\
+    sudo modprobe -r k2 ;\
+    sudo cp k2.c Makefile dkms.conf k2_trace.h /usr/src/k2-0.0.1/ &&\
+    sudo install -m 644 k2_trace.h /lib/modules/$(uname -r)/build/include/trace/events/k2.h &&\
+    sudo dkms remove k2/0.0.1 ;\
+    sudo dkms install --force k2/0.0.1 &&\
+    sudo modprobe k2 &&\
+    echo "k2" | sudo tee /sys/class/block/nvme0n1/queue/scheduler
 
 
