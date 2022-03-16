300db439623ea87b367b0b118274921a24990fbc
Georg Graßnick <georg.grassnick@tu-dresden.de>
2022-03-16 12:33:02 +0000

    Add k2.h to dkms install

--- ubuntu_reload_module.sh
+++ ubuntu_reload_module.sh
@@ -2,7 +2,7 @@
 
 echo "none" | sudo tee /sys/class/block/nvme0n1/queue/scheduler ;\
     sudo modprobe -r k2 ;\
-    sudo cp k2.c Makefile dkms.conf k2_trace.h /usr/src/k2-0.0.1/ &&\
+    sudo cp k2.c k2.h Makefile dkms.conf k2_trace.h /usr/src/k2-0.0.1/ &&\
     sudo install -m 644 k2_trace.h /lib/modules/$(uname -r)/build/include/trace/events/k2.h &&\
     sudo dkms remove k2/0.0.1 ;\
     sudo dkms install --force k2/0.0.1 &&\
