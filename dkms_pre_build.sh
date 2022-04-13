55a1aeb70406ecd9df5c9ec18779e778000cc803
Georg Graßnick <georg.grassnick@tu-dresden.de>
2022-04-13 17:29:56 +0000

    Install trace header via dkms

---
+++ dkms_pre_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+install -m 644 k2_trace.h "/lib/modules/$kernelver/build/include/trace/events/k2.h"
--- ubuntu_reload_module.sh
+++ ubuntu_reload_module.sh
@@ -2,8 +2,8 @@
 
 echo "none" | sudo tee /sys/class/block/nvme0n1/queue/scheduler ;\
     sudo modprobe -r k2 ;\
-    sudo cp k2.c k2.h Makefile dkms.conf k2_trace.h /usr/src/k2-0.0.1/ &&\
-    sudo install -m 644 k2_trace.h /lib/modules/$(uname -r)/build/include/trace/events/k2.h &&\
+    sudo mkdir -p /usr/src/k2-0.0.1/
+    sudo cp k2.c k2.h Makefile dkms.conf k2_trace.h ringbuf.h dkms_pre_build.sh /usr/src/k2-0.0.1/ &&\
     sudo dkms remove k2/0.0.1 ;\
     sudo dkms install --force k2/0.0.1 &&\
     sudo modprobe k2 &&\
